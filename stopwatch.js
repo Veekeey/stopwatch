7f56bfcc7ff409d0eaf080801bfdc7cf4d81db56
Veekeey <meligavictoria@gmail.com>
2022-04-25 12:19:44 +0100

    commit

--- stopwatch.js
+++ stopwatch.js
@@ -4,18 +4,17 @@ const numbercontainer = document.getElementById("number");
 const btn = document.querySelector(".btn");
 const content = document.querySelector(".content");
 
-
+numbercontainer.innerHTML = 30;
 btn.addEventListener("click", ()=>{
-    numbercontainer.innerHTML = 30;
     setInterval(()=>{
         numbercontainer.innerHTML-=1;
         if(numbercontainer.innerHTML < 0){
             numbercontainer.innerHTML = 0;
         }
-            if(numbercontainer.innerHTML % 2 == 0){
-                content.style.backgroundColor = "pink";
-            }else{
-                content.style.backgroundColor = "blueviolet";
-            }
+            // if(numbercontainer.innerHTML % 2 == 0){
+            //     content.style.backgroundColor = "pink";
+            // }else{
+            //     content.style.backgroundColor = "blueviolet";
+            // }
     }, 1000)
 });
